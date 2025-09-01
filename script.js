3bd2e5e287c4afa6df255dcabfd3dd07160fee61
PhilosopherMan <108624238+Philosopher-Man@users.noreply.github.com>
2025-09-01 17:06:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function scrollToSection(id) {
+    const section = document.getElementById(id);
+    section.scrollIntoView({ behavior: "smooth" });
+}
+
+function openEmail() {
+    window.location.href = "mailto:your.email@example.com";
+}
 
